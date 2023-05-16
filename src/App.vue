7d06930cfb0cc422c264fbe37e0499cbeda7fc5d
Major-Mayer <lrdarknesss@yahoo.de>
2023-05-16 13:27:48 +0200

    change titles, add camera to tab 2

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { useBackButton, useIonRouter } from '@ionic/vue';
 import { App } from '@capacitor/app';
 
+// Closes the app when the back button is pressed and no page remains in the stack
 const ionRouter = useIonRouter();
 useBackButton(-1, () => {
   if (!ionRouter.canGoBack()) {
--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -2,22 +2,33 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-title>Collections</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <ExploreContainer name="Tab 2 page" />
+    <ion-content class="ion-padding">
+      <img :src="imageSrc" />
+      <ion-button @click="takePhoto()">Take Photo</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
-</script>
+import { IonPage, IonButton, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+  import { ref } from 'vue';
+  import { Camera, CameraResultType } from '@capacitor/camera';
+
+  const imageSrc = ref('');
+  const takePhoto = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+    });
+
+    if (image.webPath === undefined) {
+      console.error("No photo has been taken!")
+      return;
+    }
+    imageSrc.value = image.webPath;
+  };
+  </script>
