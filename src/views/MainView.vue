f8630ddc3c00f9dcb333a5078ab5db0811f53366
Major-Mayer <lrdarknesss@yahoo.de>
2023-06-21 16:43:50 +0200

    Change icons for tabs

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -4,12 +4,12 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/items">
-          <ion-icon aria-hidden="true" :icon="triangle" />
+          <ion-icon aria-hidden="true" :icon="pricetag" />
           <ion-label>Items</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/collections">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
+          <ion-icon aria-hidden="true" :icon="fileTrayStacked" />
           <ion-label>Collections</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
@@ -19,5 +19,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { pricetag, fileTrayStacked } from 'ionicons/icons';
 </script>
